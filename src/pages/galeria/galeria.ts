ebf2222235f3471795c325ff964ba1c05cabb555
Jesus Medina <medinajesus821@live.com>
2017-02-16 17:48:16 -0430

    Silde Working

--- src/pages/galeria/galeria.ts
+++ src/pages/galeria/galeria.ts
@@ -13,7 +13,8 @@ export class Galeria {
   selectedItem: any;
   icons: string[];
   titles = [];
-  items: Array<{title: string, icon: string, image : string, description : string}>;
+  images = [];
+  items: Array<{title: string, icon: string, image : Array<[{ruta : string}]>, description : string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -25,12 +26,14 @@ export class Galeria {
 
     this.titles = ['the Mighty Thor', 'The Amazing Spiderman', 'The Incredible hulk', 'El Papa de los helados', 'The Evil Devil', 'The Super Dude'];
 
+    this.images = ["../assets/images/img_2.jpg", "../assets/images/img_1.jpg"];
+
     this.items = [];
     for (let i = 1; i < 13; i++) {
       this.items.push({
         title: this.titles[Math.floor(Math.random() * this.titles.length)],
         icon: this.icons[Math.floor(Math.random() * this.icons.length)],
-        image : "../assets/images/img_2.jpg",
+        image : this.images,
         description : "lorem  ipsum bla bla bla"
       });
     }
